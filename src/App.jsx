46ca342d5aeb35122616195c2eb3b6b465492a18
Sincere <sincereclarke@pursuit.org>
2024-03-22 11:41:51 -0400

    Added the initial foundation to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { Routes, Route, Link } from 'react-router-dom'
-
-import ProtectedRoute from './Components/ProtectedRoute'
-import Register from './Components/Register'
-import Login from './Components/Login'
-import Dashboard from './Components/Dashboard'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+import HeaderNav from "./Components/HeaderNav";
+import LandingPage from "./Components/LandingPage";
+import FindDoctor from "./Components/FindDoctor";
+import MotivationalQuotes from "./Components/MotivationalQuotes";
+import AboutUs from "./Components/AboutUs";
+import Resources from "./Components/Resources";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <div>
-            <h1>Welcome to the Auth Starter</h1>
-            <Link to="/login">Login</Link>
-            <h2>If you are not logged in you cannot reach this route. Try!</h2>
-            <Link to="/dashboard">Dashboard</Link>
-          </div>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route element={<ProtectedRoute />}>
-        {/* Place protected routes here */}
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
-  )
+    <BrowserRouter>
+      <HeaderNav />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/quotes" element={<MotivationalQuotes />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/find-doctor" element={<FindDoctor />} />
+          <Route path="/resources" element={<Resources />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
